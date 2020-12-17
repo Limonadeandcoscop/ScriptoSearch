e80c26d15419610fd06c163da5b5aeca3e849c6b
Franck Dupont <kyfr59@gmail.com>
2020-12-17 14:07:00 +0100

    Remove entity manager flush to avoid issues on edition page

--- Module.php
+++ Module.php
@@ -3,6 +3,8 @@ namespace ScriptoSearch;
 
 use Omeka\Module\AbstractModule;
 use Laminas\Mvc\MvcEvent;
+use Laminas\EventManager\Event;
+use Laminas\EventManager\SharedEventManagerInterface;
 
 class Module extends AbstractModule
 {
@@ -24,6 +26,41 @@ class Module extends AbstractModule
         $this->services = $this->getServiceLocator();
         $this->api = $this->getServiceLocator()->get('Omeka\ApiManager');
         $this->em = $this->getServiceLocator()->get('Omeka\EntityManager');
+    }
+
+
+    public function attachListeners(SharedEventManagerInterface $sharedEventManager)
+    {
+         $sharedEventManager->attach(
+            'Scripto\Api\Adapter\ScriptoMediaAdapter',
+            'api.hydrate.post',
+            [$this, 'updateScriptoMediaIndexation']
+        );
+    }
+
+    public function updateScriptoMediaIndexation($event) {
+
+        $request = $event->getParam('request');
+        $serviceLocator = $this->getServiceLocator();
+        $api = $serviceLocator->get('Omeka\ApiManager');
+
+        $scriptoMediaEntity = $event->getParam('entity');
+
+        $requestResource = $request->getResource();
+
+        $searchIndexes = $api->search('search_indexes')->getContent();
+
+        foreach ($searchIndexes as $searchIndex) {
+            $searchIndexSettings = $searchIndex->settings();
+            if (in_array($requestResource, $searchIndexSettings['resources'])) {
+                $indexer = $searchIndex->indexer();
+                if ($request->getOperation() == 'delete') {
+                    // TODO
+                } else {
+                    $indexer->indexMedia($scriptoMediaEntity);
+                }
+            }
+        }
     }
 }
 
--- config/module.config.php
+++ config/module.config.php
@@ -2,4 +2,14 @@
 namespace ScriptoSearch;
 
 return  [
+  'search_adapters' => [
+    'factories' => [
+        'scripto_media' => Service\AdapterFactory::class,
+    ],
+  ],
+  'solr_value_extractors' => [
+    'factories' => [
+      'scripto_media' => Service\ValueExtractor\ScriptoMediaValueExtractorFactory::class,
+    ],
+  ],
 ];
